1fbe22d3a2dcbd1ead1910d97815c5f865126527
SiDevesh <sid.swapnildevesh@gmail.com>
2016-10-16 09:01:43 +0530

    Time in IST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def timenow
-  	render json:  Time.now.strftime('%H')+":"+Time.now.strftime('%M')
+  	render json:  (Time.now+19800).strftime('%H')+":"+(Time.now+19800).strftime('%M')
   end
 
 end
